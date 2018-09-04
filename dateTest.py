417b06e60e32180aa3e2368771c0d924a91539af
Brad Schoenrock <c-brad.schoenrock@charter.com>
2018-09-04 17:25:58 -0600

    adding

--- dateTest.py
+++ dateTest.py
@@ -6,11 +6,13 @@ from datetime import timedelta
 
 
 NowDate = datetime.datetime.utcnow().strftime("%Y%m%d")
-ThenDate = (datetime.datetime.utcnow() - timedelta(hours=float(sys.argv[1]))).strftime("%Y%m%d")
+#ThenDate = (datetime.datetime.utcnow() - timedelta(hours=float(sys.argv[1]))).strftime("%Y%m%d")
+
+
 
 print (NowDate)
-print (ThenDate)
+#print (ThenDate)
 
-
+print (datetime.datetime.now.date)
 
 
