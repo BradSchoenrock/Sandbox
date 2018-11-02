aaa34db5adfae839ac769b6c4e2c0a8be5c916b2
bschoenrock <bschoenrock@localhost.localdomain>
2018-11-02 09:24:52 -0600

    updating git repo before day 2 of chef training

--- chef/cookbooks/apache/recipes/server.rb
+++ chef/cookbooks/apache/recipes/server.rb
@@ -8,15 +8,8 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1> Hello, World!</h1>
-  <h4>IPADDRESS: #{node['ipaddress']}</h4>
-  <h4>HOSTNAME: #{node['hostname']}</h4>
-  <h4>MEMORY: #{node['memory']['total']}</h4>
-  <h4>CPU: #{node['cpu']['0']['mhz']}</h4>
-
-  "
-  
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
   action :create
 end
 
