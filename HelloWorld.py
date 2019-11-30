8e0ff1a9cd56c9fd19f60964a1c3dea813625038
BradSchoenrock <brads@ucar.edu>
2019-11-30 16:50:03 -0700

    updating from laptop

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,14 @@
-print "Hello, World!"
+print ("Hello, World!")
 
 a=7
 b="stuff"
 
-print "I'm here!"
+print ("I'm here!")
 
-print "Now i'm here!"
+print ("a=",a)
 
-print "Now i'm done!"
+print ("Now i'm here!")
+
+print ("b=",b)
+
+print ("Now i'm done!")
